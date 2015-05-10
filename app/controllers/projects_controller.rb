8fa1de1326d8ab0f68ac0116b58afa6ad5d7b05d
marcin <wiercioch1989@gmail.com>
2015-05-10 18:57:29 +0000

    Project model and show view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,18 @@ class ProjectsController < ApplicationController
     end
     
     def create
-        render plain: params[:project].inspect
+        @project = Project.new(article_params)
+ 
+        @project.save
+        redirect_to @project
     end
+    
+    def show
+        @project = Project.find(params[:id])
+    end
+    
+    private
+        def article_params
+            params.require(:project).permit(:title, :text)
+        end
 end
