d5c348fa5081e8a2d7ad93720b14c4c712b1ab91
marcin <wiercioch1989@gmail.com>
2015-05-12 11:27:17 +0000

    Edit Destroy project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,22 +7,46 @@ class ProjectsController < ApplicationController
         @project = Project.new
     end
     
-    def create
-        @project = Project.new(article_params)
- 
-        if @project.save
-            redirect_to @project
-        else
-            render 'new'
-        end
-    end
-    
     def show
         @project = Project.find(params[:id])
     end
     
+    
+    def create
+        @project = Project.new(project_params)
+ 
+        if @project.save
+          redirect_to @project
+        else
+          render 'new'
+        end
+    end
+    
+    def edit
+        @project = Project.find(params[:id])
+    end
+    
+    def update
+      @project = Project.find(params[:id])
+     
+      if @project.update(project_params)
+        redirect_to @project
+      else
+        render 'edit'
+      end
+    end
+    
+    def destroy
+      @project = Project.find(params[:id])
+      @project.destroy
+     
+      redirect_to projects_path
+    end
+    
+   
+    
     private
-        def article_params
+        def project_params
             params.require(:project).permit(:title, :text)
         end
 end
